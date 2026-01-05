fe52a453ac449e86490f82b87188af6a5795de41
utkarsh-koge <161916295+utkarsh-koge@users.noreply.github.com>
2026-01-05 11:33:56 +0530

    Add files via upload

--- healthz.tsx
+++ healthz.tsx
@@ -2,5 +2,6 @@ import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 
 export async function loader({ request }: LoaderFunctionArgs) {
+    console.log("Healthcheck hit");
     return json({ status: "ok" }, { status: 200 });
 }
